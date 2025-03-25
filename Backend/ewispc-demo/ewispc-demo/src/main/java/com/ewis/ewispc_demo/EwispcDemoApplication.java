82ae9f5c48cee0f52849f8d535b0c93e37a8d2d4
Akila <akilaewis@gmail.com>
2025-03-25 09:26:56 +0530

    Initial commit

---
+++ Backend/ewispc-demo/ewispc-demo/src/main/java/com/ewis/ewispc_demo/EwispcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ewis.ewispc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwispcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwispcDemoApplication.class, args);
+	}
+
+}
---
+++ Backend/ewispc-demo/ewispc-demo/src/test/java/com/ewis/ewispc_demo/EwispcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewis.ewispc_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwispcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
